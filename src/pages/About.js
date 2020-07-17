5bf34f7ac8df6e8d6468ff2dd319ff429ba98b7f
Dejan <53543597+twinDejan@users.noreply.github.com>
2020-07-17 10:55:02 +0200

    Adding AppBar

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Typography from '@material-ui/core/Typography';
 
 const About = () => {
     return(
-        <h1>About Page</h1>
+        <div>
+        <AppBar position="static">
+            <Typography variant="h6">
+            About Page
+            </Typography>
+        </AppBar>
+        <div>
+            Content About Page
+        </div>
+    </div>
     )
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Typography from '@material-ui/core/Typography';
 
 const Home = () => {
-    return(
-        <h1>Home Page</h1>
-    )
+    return(<div>
+        <AppBar position="static">
+            <Typography variant="h6">
+            Home Page
+            </Typography>
+        </AppBar>
+        <div>
+            Content Home Page
+        </div>
+    </div>)
 }
 
 export default Home;
